7f0e0b2d5c64d91cd5174493336082593410307e
je33ica <je331687@gmail.com>
2021-01-25 22:15:23 +0000

    added util library

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer")
 const cTable = require('console.table');
+var util = require('util');
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "",
   database: "employeeTracker"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    artistSearch();
+  });
