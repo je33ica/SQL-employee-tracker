4bb6877860d8a862089b787f2ccd28c355268017
je33ica <je331687@gmail.com>
2021-01-25 11:26:19 +0000

    database seed created

---
+++ employeeTracker.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeTracker;
+
+CREATE DATABASE employeeTracker;
+
+USE employeeTracker;
+
+CREATE TABLE department(
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NULL,
+  
+);
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    
+)
