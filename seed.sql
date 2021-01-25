b23f562d7c1d59cc06742f09ab0b2be3142dbf77
je33ica <je331687@gmail.com>
2021-01-25 11:50:50 +0000

    changed name as per directions in read me

--- seed.sql
+++ seed.sql
@@ -1,3 +1,8 @@
+-- NAVIGATE TO /Users/jessicaokeeffe/Desktop/Bootcamp-Homework/SQL-employee-tracker
+-- Start the CLI with:
+-- mysql -u root -p
+-- Enter password and copy the below code upto but not including the next comment
+
 DROP DATABASE IF EXISTS employeeTracker;
 
 CREATE DATABASE employeeTracker;
@@ -22,5 +27,12 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    
-)
+);
+SET GLOBAL local_infile=1;
+-- COPY UP TO HERE^
+-- Stop the CLI you have open using:
+-- quit
+-- Restart the CLI with:
+-- mysql --local-infile=1 -u root -p
+
+
