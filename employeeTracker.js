7039aab235bbfa94c636ec7269b27aa3d4b6410d
je33ica <je331687@gmail.com>
2021-01-27 14:17:05 +0000

    started Q's for inquirer

--- employeeTracker.js
+++ employeeTracker.js
@@ -16,3 +16,48 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId + "\n");
     artistSearch();
   });
+
+//   * Add departments, roles, employees
+
+//   * View departments, roles, employees
+const startTracker = () => {
+    inquirer.promt([
+        {
+        type: "list",
+        name: "start",
+        message: "What would you like to do?",
+        choices:[
+            {
+                name:"View Departments",
+                value: displayDepartments
+              },
+              {
+                name:"View Roles",
+                value: displayRoles
+              },
+              {
+                name:"View Employees",
+                value: displayEmployees
+              },
+              {
+                name:"Exit",
+                value: exit
+              }
+        ],
+
+        }
+    ])
+    .then(function (value) {
+        if (value.displayDepartments){
+            displayDepartments();
+        } else if (value.displayRoles){
+            displayRoles();
+        } else if (value.displayEmployees){
+            displayEmployees();
+        }else (value.exit)
+        })
+}
+
+
+
+//   * Update employee roles
