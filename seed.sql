54a1b4541f8ea71906c3f4c912bfa6487512c77c
je33ica <je331687@gmail.com>
2021-01-29 19:48:57 +0000

    adding more routes to view DB

--- seed.sql
+++ seed.sql
@@ -17,6 +17,19 @@ VALUES
 ("Chardonay", "White", 5, 4), ("Moe", "Jito", 6, 4), 
 ("Hugh", "Jass", 7, 4), ("Sam", "Sung", 8, 4);
 
+--view roles
+SELECT * FROM employeeTracker.roles;
+SELECT  title FROM roles
+
+--view departments
+SELECT * FROM employeeTracker.department;
+SELECT first_name, last_name FROM employee;
+
+SELECT dept_name FROM department
+
+--view employees
+SELECT * FROM employeeTracker.employee;
+
 SELECT department.id, dept_name, roles.id, roles.title, roles.salary 
 FROM department JOIN roles ON department.id = roles.department_id
 
@@ -25,86 +38,12 @@ employee.role_id, employee.manager_id, employee.first_name,
 employee.last_name 
 FROM roles JOIN employee ON roles.id = employee.role_id
 
+--view employees
 SELECT roles.id, roles.title, roles.salary, employee.first_name, 
 employee.last_name, employee.role_id, employee.manager_id 
 FROM roles JOIN employee ON roles.id = employee.role_id;
 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
--- VALUES 
--- (1,"Larri", "Etta", 1, 1), (2,"Corbyn", "Dallas", 2, 1), 
--- (3,"Sam", "Wich", 3, 2), (4,"Lila" "Moon", 4, 1), 
--- (5,"Chardonay", "White", 5, 4), (6,"Moe", "Jito", 6, 4), 
--- (7,"Hugh", "Jass", 7, 4), (8,"Sam", "Sung", 8, 4);
-
-
-
-
--- show ALL books with authors
--- INNER JOIN will only return all matching values from both tables
-   
-SELECT * FROM employee;
-SELECT title, firstName, lastName
-FROM books
-INNER JOIN authors ON books.authorId = authors.id;
-
--- INSERT INTO employee (first_name, last_name, role_id)
--- VALUES 
--- ("Larri", "Etta", 1), ("Corbyn", "Dallas", 2), 
--- ("Sam", "Wich", 3), ("Lila" "Moon", 4); 
-
-
--- ("Chardonay", "White", 5, 4), ("Moe", "Jito", 6, 4), 
--- ("Hugh", "Jass", 7, 4), ("Sam", "Sung", 8, 4);
-
-
-
--- -- INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- -- VALUES 
--- -- ("Sam", "Wich", 5, 1) ,("Lila" "Moon", 6, 2) ,
-
--- -- ("Sam", "Sung", 8);
-
-
-
-
--- -- INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- -- VALUES 
--- -- ("Sam", "Wich", 1) ,
--- -- ("Lila" "Moon", 2) ,
--- --  ("Moe", "Jito", 3), 
--- -- ("Sam", "Sung", 3);
-
-
--- -- INSERT INTO roles (title, salary, department_id)
--- -- VALUES ("Owner", 50000, 1 ), ("Executive chef", 40000, 1,) , 
--- --  ("Head chef", 28000, 2), ("Sous chef", 25000, 2) ,   
--- --  ("sommelier ",28000, 3), ("Bar tender", 18000, 3) , 
--- --  ("HR manager",27000, 4), ("receptionist", 16000, 4);
-
--- -- INSERT INTO employee (first_name, last_name, role_id)
--- -- VALUES ("Larri", "Etta", 1), 
--- -- ("Corbyn", "Dallas", 2),   
--- -- ("Chardonay", "White", 3),
--- -- ("Hugh", "Jass", 4);
-
--- -- INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- -- VALUES 
--- -- ("Sam", "Wich", 1) ,
--- -- ("Lila" "Moon", 2) ,
--- --  ("Moe", "Jito", 3), 
--- -- ("Sam", "Sung", 3);
-
-
-
--- -- ("Larri", "Etta", 1, 1), ("Sam", "Wich", 2, 2), ("Chardonay", "White", 3), ("Hugh", "Jass", 4);
-
-
-
-
--- --        ("managment"1,          "back of house"2,           "front of house"3,         "HR"4);
--- -- VALUES ("Owner", 50000, 1,1), ("Executive chef", 40000, 1,), ("Head chef", 28000, 2), ("Sous chef", 25000, 2), ("chef de partie", 18000, 2), ("commis chef", 13000,2), 
--- -- VALUES ("Bob","Smith",1 ,1), ("Larri", "Etta", 1, 1), ("Stella", "Thorn", 2, 1), ("Corbyn", "Dallas", 2, 1), ("Lila" "Moon", 2, 1), ("Sam", "Wich", 2, 2)
-
--- -- ("managment"1,                   "back of house"2,          "front of house"3,          "HR"4);
--- -- ("Restaurant manager", 35000, 1), ("sommelier ",28000, 3), ("waiter", 16000, 3), ("Bar tender", 18000, 3), ("HR manager",27000, 4), ("receptionist", 16000, 4);
--- -- ("Harry", "Potter",1)("Chardonay", "White", 3)("Alexi", "Stroop", 3)("Moe", "Jito", 3)("Sam", "Sung", 3)("Hugh", "Jass", 4);
+-- title role and department 
+SELECT roles.title, dept_name AS department FROM roles LEFT JOIN department ON roles.department_id=department.id;
+--Name and title
+SELECT employee.first_name, employee.last_name, roles.title FROM roles JOIN employee ON roles.id = employee.role_id;
