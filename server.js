3fa66c125a8e386c4b16c865d66d2eea06d697f2
je33ica <je331687@gmail.com>
2021-01-29 21:26:26 +0000

    added figlet & chalk

--- server.js
+++ server.js
@@ -2,30 +2,82 @@ const mysql = require("mysql");
 const inquirer = require("inquirer")
 const cTable = require('console.table');
 var util = require('util');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Dollface16*",
+    password: "",
     database: "employeeTracker"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    showEmployee();
-    showDept();
-    showRoles();
+   // showEmployee();
+  //  showDept();
+ showRoles();
+    //renderImage();
+    bigText()
     
 });
+
+
+function bigText () {
+  figlet.text('Employee', {
+     font: 'ANSI Regular',
+     horizontalLayout: 'default',
+     verticalLayout: 'default'
+ }, function(err, data){
+     if (err){
+         console.log('Something went wrong...');
+         console.dir(err);
+         return;
+     };
+     console.log(chalk.blue(data));
+ });
+ figlet.text('Tracker', {
+     font: 'ANSI Regular',
+     horizontalLayout: 'default',
+     verticalLayout: 'default'
+ }, function(err, data){
+     if (err){
+         console.log('Something went wrong...');
+         console.dir(err);
+         return;
+     };
+     console.log(chalk.green(data));
+ 
+   //  mainPrompts();
+ });
+}
+
+
+//view all employess
+
+//view all employess by department
+// --  which department would you like to see employess for
+
+//view all employess by manager
+
+//add employee - first name, last name, role id, manager id
+//remove employee
+//Update employee role 
+//update employee manager
+function mainPrompts(){
+
+}
 function showEmployee() {
-    connection.query('SELECT * FROM employee', function (err, res) {
-      if (err) throw err;
+    connection.query(
+      "SELECT roles.id, roles.title, roles.salary, employee.first_name, employee.last_name, employee.manager_id FROM roles JOIN employee ON roles.id = employee.role_id" , function (err, res) {
+       // "SELECT roles.id, employee.first_name, employee.last_name, roles.title,roles.department_id, roles.salary, employee.manager_id, FROM roles JOIN employee ON roles.id = employee.role_id",function (err, res) {
+        if (err) throw err;
   
       // Log all results of the SELECT statement
-      console.table("these are the emplooyees",res);
+      console.table("these are the employees",res);
       
     });
   }
@@ -34,21 +86,22 @@ function showEmployee() {
       if (err) throw err;
   
       // Log all results of the SELECT statement
-      console.table("these are the depst",res);
+      console.table("these are the depts",res);
       
     });
   }
   function showRoles() {
-    connection.query('SELECT * FROM roles', function (err, res) {
+    connection.query('SELECT title FROM roles', function (err, res) {
       if (err) throw err;
   
       // Log all results of the SELECT statement
-      console.table("these are the rolces", res);
+      console.table("these are the roles", res);
       
     });
   }
 
-  // function addTables(){
-  //   connection.query('SELECT ')
+  // function queryEmployees(){
+  //  const query = 
   // }
+
 
