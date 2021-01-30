b9282eda5204bdf9788459e78962676181ec2afc
je33ica <je331687@gmail.com>
2021-01-30 17:19:52 +0000

    moved connection to seperate file

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+const cTable = require('console.table');
+const util = require('util');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Dollface16*",
+    database: "employeeTracker"
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
+
+
+
+  
